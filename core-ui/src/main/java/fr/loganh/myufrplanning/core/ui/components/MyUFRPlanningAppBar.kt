818fcfa8138667c30483b4d205ac8d9f71956e5b
Logan-Developer <logan2.humbert@gmail.com>
2022-06-02 16:20:47 +0200

    core-ui: Add AppBar context menu

--- core-ui/src/main/java/fr/loganh/myufrplanning/core/ui/components/MyUFRPlanningAppBar.kt
+++ core-ui/src/main/java/fr/loganh/myufrplanning/core/ui/components/MyUFRPlanningAppBar.kt
@@ -5,21 +5,34 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import fr.loganh.myufrplanning.core.ui.R
 
 @ExperimentalMaterial3Api
 @Composable
-fun MyUFRPlanningAppBar(title: String, content: @Composable() () -> Unit) {
+fun MyUFRPlanningAppBar(title: String, content: @Composable () -> Unit) {
+    var showMenu by remember { mutableStateOf(false) }
+
     Scaffold (
         topBar = {
             SmallTopAppBar (
                 title = { Text(text = title) },
                 actions = {
-                    IconButton(onClick = { /*TODO*/ }) {
+                    IconButton(onClick = { showMenu = !showMenu }) {
                         Icon(
                             Icons.Default.MoreVert,
-                            contentDescription = "More options"
+                            contentDescription = stringResource(R.string.content_desc_app_bar_menu)
+                        )
+                    }
+                    DropdownMenu(
+                        expanded = showMenu,
+                        onDismissRequest = { showMenu = false }
+                    ) {
+                        DropdownMenuItem(
+                            text = { Text(stringResource(R.string.settings)) },
+                            onClick = { /*TODO*/ }
                         )
                     }
                 }

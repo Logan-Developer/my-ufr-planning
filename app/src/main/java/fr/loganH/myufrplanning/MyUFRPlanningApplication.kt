81d2ca0a5db700f95eb890d498ef52942ad5c680
Logan-Developer <logan2.humbert@gmail.com>
2022-08-05 16:45:37 +0200

    Import and configure Hilt

---
+++ app/src/main/java/fr/loganH/myufrplanning/MyUFRPlanningApplication.kt
@@ -0,0 +1,7 @@
+package fr.loganH.myufrplanning
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyUFRPlanningApplication: Application()
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,9 @@ plugins {
     id ("com.android.library") version "7.3.0-beta05" apply false
     id ("org.jetbrains.kotlin.android") version "1.7.0" apply false
 }
+
+buildscript {
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.43.2")
+    }
+}
